a651f9f8d4997ad86e5925cb8c666c012cdc9298
s21aeshmawy <aeshmawy@sabanciuniv.edu>
2021-05-01 08:21:24 +0300

    modified:   app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ cors(
   app.all('*', function(req, res, next) {
 
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
